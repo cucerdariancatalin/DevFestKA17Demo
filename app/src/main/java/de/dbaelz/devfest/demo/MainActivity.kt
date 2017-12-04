47b69880ceb1950b41691fc1fbed68a602cb8160
Daniel Bälz <me@dbaelz.de>
2017-12-04 15:49:30 +0100

    Kotlin Android Extensions and Function Blocks

--- app/src/main/java/de/dbaelz/devfest/demo/MainActivity.kt
+++ app/src/main/java/de/dbaelz/devfest/demo/MainActivity.kt
@@ -3,28 +3,26 @@ package de.dbaelz.devfest.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import de.dbaelz.devfest.demo.data.UserService
-import de.dbaelz.devfest.demo.data.Username
-import de.dbaelz.devfest.demo.data.hasPassed
-import de.dbaelz.devfest.demo.data.measureTimeInMillis
+import android.util.TypedValue
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var lastText: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dbaelz: Username = "dbaelz"
-
-        val duration = measureTimeInMillis {
-            val user = UserService.getUser(username = dbaelz)
-            Log.d("DevFest", "User ${user.username} has ${user.points} points and " +
-                    if (user.hasPassed()) "passed" else "failed")
+        hello.apply {
+            text = "Hello DevFest Karlsruhe"
+            setTextColor(resources.getColor(R.color.colorAccent))
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+        }.also {
+            lastText = it.text.toString()
         }
-        Log.d("DevFest", "Duration is $duration ms")
 
-        val (username, _, _, latlong) = UserService.getUser(username = dbaelz)
-        val (lat, long) = latlong
-        Log.d("DevFest", "User $username location is lat $lat and long $long")
+        lastText?.let {
+            Log.d("DevFest", it)
+        }
     }
 }
